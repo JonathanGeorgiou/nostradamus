042bc203f626fd5e125ec63f2c9096c3ef914417
Jonathan Georgiou <j.georgiou94@gmail.com>
2023-04-03 21:56:26 +0100

    FS-12: Final commit

--- sql/init.sql
+++ sql/init.sql
@@ -27,6 +27,35 @@ CREATE TABLE IF NOT EXISTS "prediction" (
   "result" integer
 );
 
+CREATE TABLE IF NOT EXISTS "team" (
+  "id" serial PRIMARY KEY,
+  "full_name" varchar,
+  "short_name" varchar,
+  "three_letter_name" varchar
+);
+
+INSERT INTO TEAM (full_name, short_name, three_letter_name) VALUES
+('Arsenal', 'Arsenal', 'ARS'), 
+('Aston Villa', 'Villa', 'AVL'),
+('Bournemouth', 'Bournemouth', 'BOU'),
+('Brentford', 'Brentford', 'BRE'),
+('Brighton and Hove Albion', 'Brighton', 'BRI'),
+('Chelsea', 'Chelsea', 'CHL'),
+('Crystal Palace', 'Palace', 'CRY'),
+('Everton', 'Everton', 'EVE'),
+('Fulham', 'Fulham', 'FUL'),
+('Leeds United', 'Leeds', 'LEE'),
+('Leicester City', 'Leicester', 'LEI'),
+('Liverpool', 'Liverpool', 'LIV'),
+('Manchester City', 'Man City', 'MCY'),
+('Manchester United', 'Man United', 'MUN'),
+('Newcastle United', 'Newcastle', 'NEW'),
+('Nottingham Forest', 'Forest', 'NOT'),
+('Southampton', 'Southampton', 'SOU'),
+('Tottenham Hotspur', 'Spurs', 'TOT'),
+('West Ham United', 'West Ham', 'WHU'),
+('Wolverhampton Wanderers', 'Wolves', 'WOL');
+
 COMMENT ON COLUMN "fixture"."result" IS '0: home win, 1: away win, 2: draw';
 
 ALTER TABLE "player" ADD FOREIGN KEY ("id") REFERENCES "prediction" ("player_id");
